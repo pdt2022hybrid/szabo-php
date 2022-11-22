e5a2cf0ca423134a6a76d501d782540e26869242
alegzzz <118197474+alegzzz@users.noreply.github.com>
2022-11-22 08:11:20 +0100

    Update index.php

--- idk/index.php
+++ idk/index.php
@@ -9,35 +9,36 @@
 <body>
 
     <?php 
-        echo "ahoj <br>";
 
-
-        function arrivals() {
-            date_default_timezone_set ("Europe/Bratislava");
-            $date = date("H.i.s ");
-            echo $date;
+    function arrivals($date) {
 
             if ($date < 8) {
                 $inTime = "Nacas";
                 echo "Nacas";
+                return "OK";
             }
             elseif  ($date > 8) {
                 $late = " Meskanie";
                 echo "Meskanie";
+                return "LATE";
             }
             elseif ($date >=20) {
                 die("nemoze");
             }
 
-            function writeArrivalsIntoFile($date, $inTime, $late) {
-                file_put_contents("cas.log", "$date $inTime, $late <br>", FILE_APPEND);
-            }
-            
-
+        }
+    
+        function writeArrivalsIntoFile($date, $inTime) {
+            file_put_contents("cas.log", "$date $inTime <br>", FILE_APPEND);
         }
 
-        arrivals();
-        writeArrivalsIntoFile();
+    
+        date_default_timezone_set ("Europe/Bratislava");
+        $date = date("H.i.s ");
+        echo $date;
+    
+        $inTime = arrivals($date);
+        writeArrivalsIntoFile($date, $inTime);
         
         
     ?>
